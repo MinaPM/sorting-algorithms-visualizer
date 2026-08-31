07c713d77153fa9228d25e54820c4060704efdd7
Mina <mina.b.ph@gmail.com>
2026-08-31 00:34:53 -0400

    Refactor BubbleSort to an iterative step-by-step implementation

--- algorithms/BubbleSort.hpp
+++ algorithms/BubbleSort.hpp
@@ -8,12 +8,72 @@
 
 class BubbleSort : public Algorithm
 {
+    void (BubbleSort::*nextStep)() = nullptr;
+    size_t outerIndex = 0, innerIndex = 0;
+    bool swapped;
+    
+    void start() override { reset(); }
+    void reset() override
+    {
+        outerIndex = 0;
+        innerIndex = 0;
+        nextStep = &BubbleSort::mainLoop;
+
+        if (array)
+            array->memoryStats.resetStats();
+    }
+
+    bool step() override
+    {
+        if (nextStep)
+        {
+            (this->*nextStep)();
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    void mainLoop()
+    {
+        if (outerIndex < (*array).length() - 1)
+        {
+            swapped = false;
+            innerIndex = 0;
+
+            nextStep = &BubbleSort::innerLoop;
+            return;
+        }
+        nextStep = nullptr;
+    }
+
+    void innerLoop()
+    {
+        if (innerIndex < (*array).length() - outerIndex - 1)
+        {
+            if ((*array)[innerIndex] > (*array)[innerIndex + 1])
+            {
+                array->swap(innerIndex, innerIndex + 1);
+                swapped = true;
+            }
+            innerIndex++;
+        }
+        else
+        {
+            outerIndex++;
+            if (!swapped)
+                nextStep = nullptr;
+            else
+                nextStep = &BubbleSort::mainLoop;
+        }
+    }
+
 public:
     BubbleSort(SmartArray<BarShape> &array) : Algorithm(array)
     {
     }
-
-    void start() override {}
 
 private:
 };
